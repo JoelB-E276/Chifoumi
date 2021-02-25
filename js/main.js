d0de5ec781bb166e14aa296f0a0503605de11dcf
JoelB-E276 <joel.beauroy@outlook.fr>
2021-02-25 22:30:00 +0100

    Branch dev_test et test de commande aléatoire

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Message d'acceuil avec boite d'invite our indiquer son nom.
+var name = prompt('Bienvenue ! Tu peux te détendre en faisant une partie de Chifoumi ! Quel est ton nom ?');
+// Réutilisation du nom avec validation
+alert ('Heureux de te faire ta connaissance'+ " " +name+' Clique sur "OK" si tu es près.');
+//
+var chifoumi = prompt('Choisis et tapes : pierre, feuille ou ciseaux puis valide avec "OK"');
+
+
+
+var play = [
+    pierre=1,
+    feuille=2,
+     ciseaux=3,]
+[Math.floor(Math.random(play)*4)]
+
+console.log (play);
+
