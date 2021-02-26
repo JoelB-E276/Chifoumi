a0b8c5a7dc0547093d8c2413ed680fbfd600e55c
JoelB-E276 <joel.beauroy@outlook.fr>
2021-02-26 10:16:03 +0100

    Chifoumi project completion.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
 //        JEU DU CHIFOUMI
-// Message d'acceuil avec boite d'invite our indiquer son nom.
-var name = prompt('Bienvenue ! Tu peux te détendre en faisant une partie de Chifoumi ! Quel est ton nom ?');
+// Message d'acceuil avec boite d'invite pour indiquer son nom.
+        var name = prompt('Bienvenue ! Tu peux te détendre en faisant une partie de Chifoumi ! Quel est ton nom ?');
 
 // Réutilisation du nom avec validation
         alert ('Heureux de te faire ta connaissance'+ " " +name+'. Clique sur "OK" si tu es près.');
 
 // Choix de l'utilisateur
-var chifoumi = prompt('Choisis et tapes : pierre, feuille ou ciseaux puis valide avec "OK"');
+        var chifoumi = prompt('Choisis en tapant : pierre, feuille ou ciseaux puis valide avec "OK"');
 
 // Affichage des choix utilisateur et machine
-        const choise = ["pierre", "feuille", "ciseaux"];
-        const randomChoise = choise[Math.floor(Math.random() * choise.length)];
+        var choise = ["pierre", "feuille", "ciseaux"];
+        var randomChoise = choise[Math.floor(Math.random() * choise.length)];
 
-console.log (prompt("Tu as choisi " +chifoumi+ ",et j'ai choisi "+randomChoise));
+       alert ("Tu as choisi " +chifoumi+ ",et j'ai choisi "+randomChoise);
+
+// Résultats du jeu dans tableau if/Else
+        var resultat = {chifoumi , randomChoise};
+    
+    if (randomChoise == chifoumi) {
+        alert  ("Egalité");
+    }
+    else if ((randomChoise === choise.indexOf(0) && randomChoise=== choise.indexOf(2)) || (randomChoise === choise.indexOf(2) && randomChoise === choise.indexOf(1)) || (randomChoise === choise.indexOf(1)) && (randomChoise === choise.indexOf(0))) {
+        alert ("Gagné");
+    }
+    else {
+        alert ("Perdu");
+    }
 
 
 
-
-// Commande en stand by
-console.log (prompt("random choise =>", randomChoise));
