b6b009353867bb7e1db3157ef9464718734ff40a
JoelB-E276 <joel.beauroy@outlook.fr>
2021-02-26 07:32:10 +0100

    Random command = ok

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
+//        JEU DU CHIFOUMI
 // Message d'acceuil avec boite d'invite our indiquer son nom.
 var name = prompt('Bienvenue ! Tu peux te détendre en faisant une partie de Chifoumi ! Quel est ton nom ?');
+
 // Réutilisation du nom avec validation
-alert ('Heureux de te faire ta connaissance'+ " " +name+' Clique sur "OK" si tu es près.');
-//
+        alert ('Heureux de te faire ta connaissance'+ " " +name+'. Clique sur "OK" si tu es près.');
+
+// Choix de l'utilisateur
 var chifoumi = prompt('Choisis et tapes : pierre, feuille ou ciseaux puis valide avec "OK"');
 
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const randomMonth = months[Math.floor(Math.random() * months.length)];
+// Affichage des choix utilisateur et machine
+        const choise = ["pierre", "feuille", "ciseaux"];
+        const randomChoise = choise[Math.floor(Math.random() * choise.length)];
 
-console.log("random month =>", randomMonth);
+console.log (prompt("Tu as choisi " +chifoumi+ ",et j'ai choisi "+randomChoise));
 
 
 
-const choise = ["pierre", "feuille", "ciseaux"];
-const randomChoise = choise[Math.floor(Math.random() * choise.length)];
 
-console.log("random choise =>", randomChoise);
+// Commande en stand by
+console.log (prompt("random choise =>", randomChoise));
