f840c5474987deea69c38b6ee4552897ca8f3da5
JoelB-E276 <joel.beauroy@outlook.fr>
2021-03-05 10:49:29 +0100

    tab

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 //        JEU DU CHIFOUMI
 // Message d'acceuil avec boite d'invite pour indiquer son nom.
-        var name = prompt('Bienvenue ! Tu peux te détendre en faisant une partie de Chifoumi ! Quel est ton nom ?');
+var name = prompt('Bienvenue ! Tu peux te détendre en faisant une partie de Chifoumi ! Quel est ton nom ?');
 
 // Réutilisation du nom avec validation
-        alert ('Heureux de te faire ta connaissance'+ " " +name+'. Clique sur "OK" si tu es près.');
+alert ('Heureux de te faire ta connaissance'+ " " +name+'. Clique sur "OK" si tu es près.');
 
 // Choix de l'utilisateur
-        var chifoumi = prompt('Choisis en tapant : pierre, feuille ou ciseaux puis valide avec "OK"');
+var chifoumi = prompt('Choisis en tapant : pierre, feuille ou ciseaux puis valide avec "OK"');
 
 // Affichage des choix utilisateur et machine
-        var choise = ["pierre", "feuille", "ciseaux"];
-        var randomChoise = choise[Math.floor(Math.random() * choise.length)];
+var choise = ["pierre", "feuille", "ciseaux"];
+var randomChoice = choise[Math.floor(Math.random() * choice.length)];
 
-       alert ("Tu as choisi " +chifoumi+ ",et j'ai choisi "+randomChoise);
+alert ("Tu as choisi " +chifoumi+ ",et j'ai choisi "+randomChoice);
 
 // Résultats du jeu dans tableau if/Else
-        var resultat = {chifoumi , randomChoise};
+var resultat = {chifoumi , randomChoice};
     
-    if (randomChoise == chifoumi) {
+    if (randomChoice == chifoumi) {
         alert  ("Egalité");
     }
-    else if ((randomChoise === choise.indexOf(0) && randomChoise=== choise.indexOf(2)) || (randomChoise === choise.indexOf(2) && randomChoise === choise.indexOf(1)) || (randomChoise === choise.indexOf(1)) && (randomChoise === choise.indexOf(0))) {
+    else if ((randomChoice === choice.indexOf(0) && randomChoice=== choice.indexOf(2)) || (randomChoice === choice.indexOf(2) && randomChoice === choice.indexOf(1)) || (randomChoice === choice.indexOf(1)) && (randomChoice === choice.indexOf(0))) {
         alert ("Gagné");
     }
     else {
